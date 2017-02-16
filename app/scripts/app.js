05545520b8a33e88e6a0ca63ba1b642a4e39ce03
David M <davidmh.mail@gmail.com>
2017-02-16 16:45:33 -0700

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        
+        $stateProvider
+            .state('main', {
+                url: '/'
+            });
+    }
+    
+    angular
+        .module('blocChat', ['ui.router', 'firebase'])
+        .config(config);
+}());
